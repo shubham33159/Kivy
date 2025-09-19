bead52cc0629335a7d91c2b7d7174e9322bb790b
shubham33159 <shubham33159@gmailcom>
2025-09-20 00:05:06 +0530

    Hello World

---
+++ Hello_World.py
@@ -0,0 +1,13 @@
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.core.window import Window
+
+Window.clearcolor=(1,1,1,1)
+
+
+class MyApp(App):
+    def build(self):
+        label = Label(text="Hello World", font_size="120sp", bold=True, italic=True, color=(0,1,0,1))
+        return label
+    
+MyApp().run()
